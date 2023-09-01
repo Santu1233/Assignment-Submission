3ff185256f7b23a1e4bb549fbae0130ac02ccd2b
Santu1233 <134389048+Santu1233@users.noreply.github.com>
2023-09-01 20:46:16 +0530

    day 2 Assignment Submitted

---
+++ day2.py
@@ -0,0 +1,19 @@
+
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+
+floor_division_result = a // b
+
+
+power_result = a ** b
+
+
+equal_result = (a == b)
+
+
+print("1. Floor Division:", floor_division_result)
+print("2. a power b:", power_result)
+print("3. Are a and b equal?", equal_result)
+
+
